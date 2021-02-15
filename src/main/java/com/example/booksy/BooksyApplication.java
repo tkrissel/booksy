8dc93828d34454babaabc87c47f6156d4697c1ad
Todd Krissel <ephbreif-github@yahoo.com>
2021-02-15 06:29:25 +0000

    Initial commit of sample Booksy project.

---
+++ src/main/java/com/example/booksy/BooksyApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booksy/BooksyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
